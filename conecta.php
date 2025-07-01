8fc9eedc372fe9b02597c85810c9866220a9f9ba
Rodrigo Oyarzún <113133630+royarzunma@users.noreply.github.com>
2025-07-01 00:51:19 -0300

    ROM_PROC_INF

--- conecta.php
+++ conecta.php
@@ -1,10 +1,10 @@
 <?php
 $host = 'localhost'; 
 $usuario = 'rodrigo'; 
-$contrasena = 'Mario.2016'; 
+$contrasena = 'Password_rodrigo'; // agregamos la contraseña de la conexión
 $base_de_datos = 'AGENCIA'; 
 $conexion = new mysqli($host, $usuario, $contrasena, $base_de_datos);
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
-$conexion->set_charset("utf8");
+$conexion->set_charset("utf8"); // agregamos compatibilidad para UTF8
